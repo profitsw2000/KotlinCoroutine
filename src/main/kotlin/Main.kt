6936a189d6be51d543beff75bfcc75be588abb6d
profitsw2000 <profitsw2000@gmail.com>
2022-05-29 17:00:12 +0300

    Coroutine launch without using Globalcope.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,17 +3,13 @@ import kotlinx.coroutines.*
 fun main() = runBlocking {
     println("Start: ${Thread.currentThread().name}")
 
-    GlobalScope.launch {
+    launch {
         println("Fake work start: ${Thread.currentThread().name}")
-        mySuspendFunc(2000)
+        delay(2000)
         println("Fake work end: ${Thread.currentThread().name}")
     }
 
-    mySuspendFunc(2500)
+    delay(2500)
 
     println("End: ${Thread.currentThread().name}")
-}
-
-suspend fun mySuspendFunc (time: Long) {
-    delay(time)
 }
