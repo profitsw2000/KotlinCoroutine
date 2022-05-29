44bc80ad8a53218615e8a772c399cd2be3ebd37f
profitsw2000 <profitsw2000@gmail.com>
2022-05-29 16:21:21 +0300

    Fake work with new thread.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,5 +2,12 @@ import kotlin.concurrent.thread
 
 fun main() {
     println("Start: ${Thread.currentThread().name}")
+
+    thread {
+        println("Fake work start: ${Thread.currentThread().name}")
+        Thread.sleep(2000)
+        println("Fake work end: ${Thread.currentThread().name}")
+    }
+
     println("End: ${Thread.currentThread().name}")
 }
