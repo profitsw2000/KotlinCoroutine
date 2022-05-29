a9ce937f19f5f632baaf7f601bc8f8bc15437d61
profitsw2000 <profitsw2000@gmail.com>
2022-05-29 21:24:04 +0300

    Coroutine block using withTimeOutOrNull.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,12 +3,14 @@ import kotlinx.coroutines.*
 fun main() = runBlocking {
     println("Start: ${Thread.currentThread().name}")
 
-    withTimeout(3000) {
+    val result: String? = withTimeoutOrNull(3000) {
         for (i in 0..10) {
             println(i)
-            delay(1000)
+            delay(100)
         }
+        "I'm done!"
     }
 
+    println("Result = $result")
     println("End: ${Thread.currentThread().name}")
 }
