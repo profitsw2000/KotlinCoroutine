cd703b458ed9ed1206014237582e4912e86bb955
profitsw2000 <profitsw2000@gmail.com>
2022-05-29 21:21:08 +0300

    Coroutine block with timeout.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,25 +3,12 @@ import kotlinx.coroutines.*
 fun main() = runBlocking {
     println("Start: ${Thread.currentThread().name}")
 
-    val job: Job = launch(Dispatchers.Default) {
-        try {
-            for (i in 0..10) {
-                println(i)
-                delay(1000)
-            }
-        } catch (ex: CancellationException) {
-            println("\nCaught exception: ${ex.message}")
-        } finally {
-            withContext(NonCancellable) {
-                delay(5000)
-                println("\nFinal block.")
-            }
+    withTimeout(3000) {
+        for (i in 0..10) {
+            println(i)
+            delay(1000)
         }
     }
-
-    delay(3000)
-    job.cancel("My exception!!")
-    job.join()
 
     println("End: ${Thread.currentThread().name}")
 }
