2a4d8c202f052e079ca67e64cc7b01c9055916ce
profitsw2000 <profitsw2000@gmail.com>
2022-05-29 17:11:24 +0300

    Use job object to work with coroutine.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,13 +3,13 @@ import kotlinx.coroutines.*
 fun main() = runBlocking {
     println("Start: ${Thread.currentThread().name}")
 
-    launch {
+    val job: Job = launch {
         println("Fake work start: ${Thread.currentThread().name}")
         delay(2000)
         println("Fake work end: ${Thread.currentThread().name}")
     }
 
-    delay(2500)
+    job.join()
 
     println("End: ${Thread.currentThread().name}")
 }
