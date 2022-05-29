7505c534ded969d4e48ffc170ea22d50b114fd90
profitsw2000 <profitsw2000@gmail.com>
2022-05-29 21:10:40 +0300

    Test withContext operator to make cooperative func work in finally block.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,12 +7,15 @@ fun main() = runBlocking {
         try {
             for (i in 0..10) {
                 println(i)
-                Thread.sleep(1000)
+                delay(1000)
             }
         } catch (ex: CancellationException) {
             println("\nCaught exception!!")
         } finally {
-            println("\nFinal block.")
+            withContext(NonCancellable) {
+                delay(5000)
+                println("\nFinal block.")
+            }
         }
     }
 
